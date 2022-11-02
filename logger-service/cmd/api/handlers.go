14f1a147a58e91508bc20b275ec7aecf56753984
Raymond Ong <rayjosong@gmail.com>
2022-11-02 16:36:50 +0800

    logger-service/ set up routes, handlers, helpers and web server

---
+++ logger-service/cmd/api/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log-service/data"
+	"net/http"
+
+	"github.com/tsawler/toolbox"
+)
+
+type JSONPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
+	var tools toolbox.Tools
+
+	// read json into var
+	var requestPayload JSONPayload
+	_ = tools.ReadJSON(w, r, &requestPayload)
+
+	// insert data
+	event := data.LogEntry{
+		Name: requestPayload.Name,
+		Data: requestPayload.Data,
+	}
+
+	err := app.Models.LogEntry.Insert(event)
+	if err != nil {
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	resp := jsonResponse{
+		Error: false,
+		Message: "logged",
+	}
+
+	toolbox.JSONResponse(w, http.StatusAccepted, resp)
+}
--- logger-service/go.mod
+++ logger-service/go.mod
@@ -3,10 +3,14 @@ module log-service
 go 1.19
 
 require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tsawler/toolbox v1.1.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
